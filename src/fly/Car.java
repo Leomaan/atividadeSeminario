82132b182aec7d8f44c5aa5fa9a44ee10fe3d3f9
Leoman Cássio <leoman.cassio@hotmail.com>
2025-05-08 12:09:19 -0300

    atv2 - fly

--- src/fly/Car.java
+++ src/fly/Car.java
@@ -5,17 +5,6 @@ public class Car {
     private String yearOfManuFacture;
     private CarType carType;
 
-    /* LISTA DE ATRIBUTOS DO CARRO
-     * - licensePlate
-     * - chassisNumber
-     * - model
-     * - brand
-     * - yearOfManufacture
-     * - price
-     *
-     * DISTRIBUA OS ATRIBUTOS ENTRE Car E CarType, OBSERVANDO A NATUREZA DE CADA ATRIBUTO
-     * */
-
     // CONSTRUTOR
     public Car(CarType carType) {
         this.carType = carType;
--- src/fly/CarType.java
+++ src/fly/CarType.java
@@ -6,17 +6,6 @@ public class CarType {
     private String model;
     private float price;
 
-    /* LISTA DE ATRIBUTOS DO CARRO
-     * - licensePlate
-     * - chassisNumber
-     * - model
-     * - brand
-     * - yearOfManufacture
-     * - price
-     *
-     * DISTRIBUA OS ATRIBUTOS ENTRE Car E CarType, OBSERVANDO A NATUREZA DE CADA ATRIBUTO
-     * */
-
     // CONSTRUTOR
     public CarType(String model) {
         this.model = model;
