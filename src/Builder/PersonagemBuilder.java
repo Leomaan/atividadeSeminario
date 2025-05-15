aaf4eef4da80ebfc45fa1996f9a3126205c79e31
Leoman Cássio <leoman.cassio@hotmail.com>
2025-05-15 11:54:38 -0300

    atv7 - Builder

---
+++ src/Builder/PersonagemBuilder.java
@@ -0,0 +1,16 @@
+package Builder;
+
+public interface PersonagemBuilder {
+
+    //Faça o restante do builder
+
+    PersonagemBuilder comNome(String nome);
+    PersonagemBuilder comVida(int vida);
+    PersonagemBuilder comMana(int mana);
+    PersonagemBuilder comForca(int forca);
+    PersonagemBuilder comInteligencia(int inteligencia);
+    PersonagemBuilder comAgilidade(int agilidade);
+
+
+    Personagem build();
+}
