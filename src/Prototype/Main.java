55c62e555963d67a0bf16af665e73d8e10d79234
Leomaan <leoman.cassio@hotmail.com>
2025-05-14 21:40:47 -0300

    atv6 - Prototype

---
+++ src/Prototype/Main.java
@@ -0,0 +1,20 @@
+package Prototype;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando inimigo original
+        EnemyPrototype enemy = new Enemy(1000, "Goblin", "demonio", "Masculino", 1200);
+        enemy.displayInfo();
+
+        // Clonando o inimigo
+        EnemyPrototype enemyCopia = enemy.clone();
+        enemyCopia.displayInfo();
+
+        // Usando Factory
+        EnemyPrototype orcClone = Factory.createEnemy("OrcGreen");
+        orcClone.displayInfo();
+
+        EnemyPrototype goblinClone = Factory.createEnemy("GoblinRed");
+        goblinClone.displayInfo();
+    }
+}
